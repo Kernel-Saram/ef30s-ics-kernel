d9adf3d571e86d100ed6d0df6693e212961343d9
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-02-01 10:09:01 -0600

    backport

--- include/linux/genalloc.h
+++ include/linux/genalloc.h
@@ -10,6 +10,7 @@
 
 #ifndef __GENALLOC_H__
 #define __GENALLOC_H__
+<<<<<<< HEAD
 struct gen_pool;
 
 struct gen_pool *__must_check gen_pool_create(unsigned order, int nid);
@@ -39,12 +40,45 @@ void gen_pool_free(struct gen_pool *pool, unsigned long addr, size_t size);
 extern phys_addr_t gen_pool_vert_to_phys(struct gen_pool *pool, unsigned long);
 extern int gen_pool_add_virt(struct gen_pool *, unsigned long, phys_addr_t, 
 		size_t, int);
+=======
+
+struct gen_pool;
+
+struct gen_pool *__must_check gen_pool_create(unsigned order, int nid);
+
+void gen_pool_destroy(struct gen_pool *pool);
+
+unsigned long __must_check
+gen_pool_alloc_aligned(struct gen_pool *pool, size_t size,
+		       unsigned alignment_order);
+
+/**
+ * gen_pool_alloc() - allocate special memory from the pool
+ * @pool:	Pool to allocate from.
+ * @size:	Number of bytes to allocate from the pool.
+ *
+ * Allocate the requested number of bytes from the specified pool.
+ * Uses a first-fit algorithm.
+ */
+static inline unsigned long __must_check
+gen_pool_alloc(struct gen_pool *pool, size_t size)
+{
+	return gen_pool_alloc_aligned(pool, size, 0);
+}
+
+void gen_pool_free(struct gen_pool *pool, unsigned long addr, size_t size);
+
+extern phys_addr_t gen_pool_virt_to_phys(struct gen_pool *pool, unsigned long);
+extern int gen_pool_add_virt(struct gen_pool *, unsigned long, phys_addr_t,
+			     size_t, int);
+>>>>>>> f3747d8... lib: bitmap/genalloc: backport from cafs 3.0
 /**
  * gen_pool_add - add a new chunk of special memory to the pool
  * @pool: pool to add new memory chunk to
  * @addr: starting address of memory chunk to add to pool
  * @size: size in bytes of the memory chunk to add to pool
  * @nid: node id of the node the chunk structure and bitmap should be
+<<<<<<< HEAD
  * 	allocated on, or -1
  *
  * Add a new chunk of special memory to the specified pool.
@@ -52,6 +86,16 @@ extern int gen_pool_add_virt(struct gen_pool *, unsigned long, phys_addr_t,
  * Returns 0 on success or a -ve errno on failure.
  */
 static inline int __must_check gen_pool_add(struct gen_pool *pool, unsigned long addr, size_t size, int nid)
+=======
+ *       allocated on, or -1
+ *
+ * Add a new chunk of special memory to the specified pool.
+ *
+ * Returns 0 on success or a -ve errno on failure.
+ */
+static inline int __must_check gen_pool_add(struct gen_pool *pool, unsigned long addr,
+			       size_t size, int nid)
+>>>>>>> f3747d8... lib: bitmap/genalloc: backport from cafs 3.0
 {
 	return gen_pool_add_virt(pool, addr, -1, size, nid);
 }
--- lib/genalloc.c
+++ lib/genalloc.c
@@ -18,27 +18,49 @@
 
 /* General purpose special memory pool descriptor. */
 struct gen_pool {
+<<<<<<< HEAD
 rwlock_t lock;	/* protects chunks list */
 struct list_head chunks;	/* list of chunks in this pool */
 unsigned order;	/* minimum allocation order */
+=======
+	rwlock_t lock;			/* protects chunks list */
+	struct list_head chunks;	/* list of chunks in this pool */
+	unsigned order;			/* minimum allocation order */
+>>>>>>> f3747d8... lib: bitmap/genalloc: backport from cafs 3.0
 };
 
 /* General purpose special memory pool chunk descriptor. */
 struct gen_pool_chunk {
+<<<<<<< HEAD
 	spinlock_t lock;	/* protects bits */
 	struct list_head next_chunk;	/* next chunk in pool */
 	phys_addr_t phys_addr;	/* physical starting address of memory chunk */
 	unsigned long start;	/* start of memory chunk */
 	unsigned long size;	/* number of bits */
 	unsigned long bits[0];	/* bitmap for allocating memory chunk */
+=======
+	spinlock_t lock;		/* protects bits */
+	struct list_head next_chunk;	/* next chunk in pool */
+	phys_addr_t phys_addr;		/* physical starting address of memory chunk */
+	unsigned long start;		/* start of memory chunk */
+	unsigned long size;		/* number of bits */
+	unsigned long bits[0];		/* bitmap for allocating memory chunk */
+>>>>>>> f3747d8... lib: bitmap/genalloc: backport from cafs 3.0
 };
 
 /**
  * gen_pool_create() - create a new special memory pool
+<<<<<<< HEAD
  * @order: Log base 2 of number of bytes each bitmap bit
  * represents.
  * @nid: Node id of the node the pool structure should be allocated
  * on, or -1. This will be also used for other allocations.
+=======
+ * @order:	Log base 2 of number of bytes each bitmap bit
+ *		represents.
+ * @nid:	Node id of the node the pool structure should be allocated
+ *		on, or -1.  This will be also used for other allocations.
+>>>>>>> f3747d8... lib: bitmap/genalloc: backport from cafs 3.0
  *
  * Create a new special memory pool that can be used to manage special purpose
  * memory not managed by the regular kmalloc/kfree interface.
@@ -74,6 +96,7 @@ EXPORT_SYMBOL(gen_pool_create);
  * Returns 0 on success or a -ve errno on failure.
  */
 int __must_check gen_pool_add_virt(struct gen_pool *pool, unsigned long virt, phys_addr_t phys,
+<<<<<<< HEAD
 	size_t size, int nid)
 {
 	struct gen_pool_chunk *chunk;
@@ -87,6 +110,21 @@ int __must_check gen_pool_add_virt(struct gen_pool *pool, unsigned long virt, ph
 	if (WARN_ON(!size))
 		return -EINVAL;
 
+=======
+		 size_t size, int nid)
+{
+	struct gen_pool_chunk *chunk;
+	size_t nbytes;
+
+	if (WARN_ON(!virt || virt + size < virt ||
+	    (virt & ((1 << pool->order) - 1))))
+		return -EINVAL;
+
+	size = size >> pool->order;
+	if (WARN_ON(!size))
+		return -EINVAL;
+
+>>>>>>> f3747d8... lib: bitmap/genalloc: backport from cafs 3.0
 	nbytes = sizeof *chunk + BITS_TO_LONGS(size) * sizeof *chunk->bits;
 	chunk = kzalloc_node(nbytes, GFP_KERNEL, nid);
 	if (!chunk)
@@ -95,7 +133,11 @@ int __must_check gen_pool_add_virt(struct gen_pool *pool, unsigned long virt, ph
 	spin_lock_init(&chunk->lock);
 	chunk->phys_addr = phys;
 	chunk->start = virt >> pool->order;
+<<<<<<< HEAD
 	chunk->size = size;
+=======
+	chunk->size  = size;
+>>>>>>> f3747d8... lib: bitmap/genalloc: backport from cafs 3.0
 
 	write_lock(&pool->lock);
 	list_add(&chunk->next_chunk, &pool->chunks);
@@ -122,8 +164,13 @@ phys_addr_t gen_pool_virt_to_phys(struct gen_pool *pool, unsigned long addr)
 		chunk = list_entry(_chunk, struct gen_pool_chunk, next_chunk);
 
 		if (addr >= chunk->start &&
+<<<<<<< HEAD
 			addr < (chunk->start + chunk->size))
 				return chunk->phys_addr + addr - chunk->start;
+=======
+		    addr < (chunk->start + chunk->size))
+			return chunk->phys_addr + addr - chunk->start;
+>>>>>>> f3747d8... lib: bitmap/genalloc: backport from cafs 3.0
 	}
 	read_unlock(&pool->lock);
 
@@ -133,7 +180,11 @@ EXPORT_SYMBOL(gen_pool_virt_to_phys);
 
 /**
  * gen_pool_destroy() - destroy a special memory pool
+<<<<<<< HEAD
  * @pool: Pool to destroy.
+=======
+ * @pool:	Pool to destroy.
+>>>>>>> f3747d8... lib: bitmap/genalloc: backport from cafs 3.0
  *
  * Destroy the specified special memory pool. Verifies that there are no
  * outstanding allocations.
@@ -145,7 +196,11 @@ void gen_pool_destroy(struct gen_pool *pool)
 
 	while (!list_empty(&pool->chunks)) {
 		chunk = list_entry(pool->chunks.next, struct gen_pool_chunk,
+<<<<<<< HEAD
 			next_chunk);
+=======
+				   next_chunk);
+>>>>>>> f3747d8... lib: bitmap/genalloc: backport from cafs 3.0
 		list_del(&chunk->next_chunk);
 
 		bit = find_next_bit(chunk->bits, chunk->size, 0);
@@ -159,18 +214,30 @@ EXPORT_SYMBOL(gen_pool_destroy);
 
 /**
  * gen_pool_alloc_aligned() - allocate special memory from the pool
+<<<<<<< HEAD
  * @pool: Pool to allocate from.
  * @size: Number of bytes to allocate from the pool.
  * @alignment_order: Order the allocated space should be
  * aligned to (eg. 20 means allocated space
  * must be aligned to 1MiB).
+=======
+ * @pool:	Pool to allocate from.
+ * @size:	Number of bytes to allocate from the pool.
+ * @alignment_order:	Order the allocated space should be
+ *			aligned to (eg. 20 means allocated space
+ *			must be aligned to 1MiB).
+>>>>>>> f3747d8... lib: bitmap/genalloc: backport from cafs 3.0
  *
  * Allocate the requested number of bytes from the specified pool.
  * Uses a first-fit algorithm.
  */
 unsigned long __must_check
 gen_pool_alloc_aligned(struct gen_pool *pool, size_t size,
+<<<<<<< HEAD
 			unsigned alignment_order)
+=======
+		       unsigned alignment_order)
+>>>>>>> f3747d8... lib: bitmap/genalloc: backport from cafs 3.0
 {
 	unsigned long addr, align_mask = 0, flags, start;
 	struct gen_pool_chunk *chunk;
@@ -182,6 +249,10 @@ gen_pool_alloc_aligned(struct gen_pool *pool, size_t size,
 		align_mask = (1 << (alignment_order - pool->order)) - 1;
 
 	size = (size + (1UL << pool->order) - 1) >> pool->order;
+<<<<<<< HEAD
+=======
+
+>>>>>>> f3747d8... lib: bitmap/genalloc: backport from cafs 3.0
 	read_lock(&pool->lock);
 	list_for_each_entry(chunk, &pool->chunks, next_chunk) {
 		if (chunk->size < size)
@@ -189,8 +260,13 @@ gen_pool_alloc_aligned(struct gen_pool *pool, size_t size,
 
 		spin_lock_irqsave(&chunk->lock, flags);
 		start = bitmap_find_next_zero_area_off(chunk->bits, chunk->size,
+<<<<<<< HEAD
 			0, size, align_mask,
 			chunk->start);
+=======
+						       0, size, align_mask,
+						       chunk->start);
+>>>>>>> f3747d8... lib: bitmap/genalloc: backport from cafs 3.0
 		if (start >= chunk->size) {
 			spin_unlock_irqrestore(&chunk->lock, flags);
 			continue;
@@ -211,9 +287,15 @@ EXPORT_SYMBOL(gen_pool_alloc_aligned);
 
 /**
  * gen_pool_free() - free allocated special memory back to the pool
+<<<<<<< HEAD
  * @pool: Pool to free to
  * @addr: Starting address of memory to free back to pool
  * @size: Size in bytes of memory to free
+=======
+ * @pool:	Pool to free to.
+ * @addr:	Starting address of memory to free back to pool.
+ * @size:	Size in bytes of memory to free.
+>>>>>>> f3747d8... lib: bitmap/genalloc: backport from cafs 3.0
  *
  * Free previously allocated special memory back to the specified pool.
  */
@@ -227,12 +309,22 @@ void gen_pool_free(struct gen_pool *pool, unsigned long addr, size_t size)
 
 	addr = addr >> pool->order;
 	size = (size + (1UL << pool->order) - 1) >> pool->order;
+<<<<<<< HEAD
 
 	BUG_ON(addr + size < addr);
 	read_lock(&pool->lock);
 	list_for_each_entry(chunk, &pool->chunks, next_chunk)
 		if (addr >= chunk->start &&
 			addr + size <= chunk->start + chunk->size) {
+=======
+
+	BUG_ON(addr + size < addr);
+
+	read_lock(&pool->lock);
+	list_for_each_entry(chunk, &pool->chunks, next_chunk)
+		if (addr >= chunk->start &&
+		    addr + size <= chunk->start + chunk->size) {
+>>>>>>> f3747d8... lib: bitmap/genalloc: backport from cafs 3.0
 			spin_lock_irqsave(&chunk->lock, flags);
 			bitmap_clear(chunk->bits, addr - chunk->start, size);
 			spin_unlock_irqrestore(&chunk->lock, flags);
